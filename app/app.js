89eaa0842a4da6febf2a99d2bea63abc2f24e686
ssontag55 <sontag.stephen@gmail.com>
2016-04-21 03:56:12 -0400

    added directions and minified

--- app/app.js
+++ app/app.js
@@ -10,24 +10,36 @@ function startup(){
 	L.mapbox.accessToken = 'pk.eyJ1Ijoic2Vkb25hY2hhbWJlciIsImEiOiJjaW13Zmp3cGswMzd0d2tsdXBnYmVjNmRjIn0.PlcjviLrxQht-_tBEbQQeg';
 
 	var map = L.mapbox.map('map').setView([34.86394, -111.764860], 14).addControl(L.mapbox.shareControl()).addControl(L.mapbox.geocoderControl('mapbox.places'));
+	that.map = map;
 
+	that.gotoloc = '';
 	var points = L.mapbox.featureLayer('sedonachamber.pmj9fija').addTo(map);
 
 	points.on('mouseover', function (e) {
-	  e.layer.openPopup();
+	  	e.layer.openPopup();
+	  	that.gotoloc = [e.layer.feature.geometry.coordinates[1],e.layer.feature.geometry.coordinates[0]];
+
+		$("#direc" ).click(function(e) {
+			e.preventDefault();
+		  	//get direction same
+		    that.map.locate();
+		});
 	});
-	var myIcon = L.icon({
-	    iconUrl: 'app/css/31.gif',
-	    iconAnchor: [8, 8]
-	})
+	
+	// var mylocIcon = L.icon({
+	//     iconUrl: 'app/css/45.gif',
+	//     iconAnchor: [8, 8]
+	// });
+
 	var lc = L.control.locate({follow: true,
 		keepCurrentZoomLevel: false,
 		locateOptions: {maxZoom: 16},
 		metric: false,
 		showPopup: true, 
-		markerClass: L.marker,
-		markerStyle: {icon:myIcon},
-	  	onLocationError: function(err) {alert(err.message)}}).addTo(map);
+		//markerClass: L.marker,
+		//markerStyle: {icon:mylocIcon},
+	  	onLocationError: function(err) {alert(err.message)}
+	}).addTo(map);
 
 	L.control.layers({
 	    'Streets': L.mapbox.tileLayer('mapbox.streets',{maxZoom:20}),
@@ -39,25 +51,30 @@ function startup(){
 	    'Light': L.mapbox.tileLayer('mapbox.light'),
 	    'Dark': L.tileLayer('https://api.mapbox.com/styles/v1/sedonachamber/cin961o3z00epcxnhaxgzwdb6/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2Vkb25hY2hhbWJlciIsImEiOiJjaW13Zmp3cGswMzd0d2tsdXBnYmVjNmRjIn0.PlcjviLrxQht-_tBEbQQeg', {maxZoom:20})
 	}, {
+		//add other layers here
 	    //'Bike Stations': L.mapbox.tileLayer('examples.bike-locations')
 	}).addTo(map);
 
 	map.on('locationfound',(function(t) {
-	        
-	        //find closest point
-	        var loc = {
-	            "type": "Feature",
-	            "properties": {
-	              "marker-color": "#f00",
-	              "marker-size": 'small'
-	            },
-	            "geometry": {
-	              "type": "Point",
-	              "coordinates": [t.longitude,t.latitude]
-	            }
-	        };
 
-	        var jsonpoints = points.getGeoJSON();
+        //find closest point
+        var loc = {
+            "type": "Feature",
+            "properties": {
+              "marker-color": "#f00",
+              "marker-size": 'small'
+            },
+            "geometry": {
+              "type": "Point",
+              "coordinates": [t.longitude,t.latitude]
+            }
+        };
+
+        that.currentloc = loc;
+
+        //for locate option
+        if(that.gotoloc ==''){
+        	var jsonpoints = points.getGeoJSON();
 	        ///filter out points
 	        for (var i = 0; i < jsonpoints.features.length; i++) {
 	          if(jsonpoints.features[i].geometry.type != 'Point'){
@@ -85,25 +102,18 @@ function startup(){
 	              //get directions  
 	              //to
 	              var tocoords=[nearestpoint.geometry.coordinates[1],nearestpoint.geometry.coordinates[0]];
-	              L.circle(tocoords, 25,{
-					    color: '#fff',
-					    fillColor: '#9E2C2E',
-					    fillOpacity: 0.7
-				   }).addTo(map);  
-
-	              //from 
-	              L.circle(t.latlng, 25,{
-					    color: '#fff',
-					    fillColor: '#A1D490',
-					    fillOpacity: 0.7
-				   }).addTo(map).bringToFront();
-	               //L.geoJson(loc).addTo(map);    
-
-	               that.getdirections(t.latlng,tocoords)
+	              
+				  that.getdirections(t.latlng,tocoords);
 	            }
 	        });
-	        map.stopLocate();
-	        //lc.stop(); 
+        }
+        //for regular directions
+        else{
+        	that.getdirections(t.latlng,that.gotoloc);
+        }
+        
+        map.stopLocate();
+        //lc.stop(); 
 	}));
 
 	//cluster
@@ -164,15 +174,67 @@ function startup(){
 	    }
 	  }
 	});
-
-	$("#direc" ).click(function() {
-	   //get direction same
-	  alert( "Handler for called." );
-	});
 }
 
 function getdirections(start,end){
 	that = this;
 
-	//https://api.tiles.mapbox.com/v4/directions/mapbox.walking/-74.8553466796875,40.029717557833266;-74.8553466796875,39.91605629078665.json?instructions=html&geometry=polyline&access_token="+that.token
+	//duration in seconds/60
+	//distance in meters/3.28084 ft
+	//distance in meters/0.000621371 miles
+
+	if(that.directionlayer){
+		that.map.removeLayer(that.directionlayer)
+	}
+	if(that.to){
+		that.map.removeLayer(that.to)
+	}
+	if(that.from){
+		that.map.removeLayer(that.from)
+	}
+	
+	that.to = L.circle(end, 25,{
+		    color: '#fff',
+		    fillColor: '#9E2C2E',
+		    fillOpacity: 0.7
+	   }).addTo(map);  
+
+	//from 
+	that.from = L.circle(start, 25,{
+		    color: '#fff',
+		    fillColor: '#A1D490',
+		    fillOpacity: 0.7
+	   }).addTo(map).bringToFront();
+	
+	//using cycling because walking doesn't give adequate directions
+	var dir_url = "https://api.tiles.mapbox.com/v4/directions/mapbox.cycling/"+start.lng+','+start.lat+";"+end[1]+','+end[0]+".json?instructions=json&geometry=line&access_token="+that.token
+	
+	$.get( dir_url, function( data ) {
+
+		if(data.routes.length>0){
+			var directions = {};
+			directions.geometry = data.routes[0].geometry;
+			directions.type="Feature";
+		    directions.properties = {};
+		    
+		    //directions.properties.popupContent		    
+		    var customPopup= "<i>Walking Time: "+Number(data.routes[0].duration/19).toPrecision(2)+" minutes<br><br>Walking Distance: "+Number(data.routes[0].distance*0.000621371).toPrecision(2)+" miles";
+			
+			var dirStyle = {
+			    "color": "#61C5BE",
+			    "weight": 5,
+			    "opacity": 0.9
+			};
+
+			that.directionlayer = L.geoJson(directions,dirStyle).bindPopup(customPopup).addTo(that.map);
+			that.directionlayer.eachLayer(function(m) {
+			  m.openPopup();
+			});
+		}
+		else{
+			alert('No Current Directions, please try again')
+		}	
+
+		that.gotoloc = '';  
+	});
 }
