8756965d5908082ea2c9971085e61ba60a4c4c80
ssontag55 <sontag.stephen@gmail.com>
2016-04-24 15:59:57 -0700

    added better styles and directions updates on mapbox

--- app/app.js
+++ app/app.js
@@ -7,54 +7,113 @@ Mapblender
 function startup(){
 	that = this;
 	that.token = 'pk.eyJ1Ijoic2Vkb25hY2hhbWJlciIsImEiOiJjaW13Zmp3cGswMzd0d2tsdXBnYmVjNmRjIn0.PlcjviLrxQht-_tBEbQQeg';
+	
 	L.mapbox.accessToken = 'pk.eyJ1Ijoic2Vkb25hY2hhbWJlciIsImEiOiJjaW13Zmp3cGswMzd0d2tsdXBnYmVjNmRjIn0.PlcjviLrxQht-_tBEbQQeg';
 
 	var map = L.mapbox.map('map').setView([34.86394, -111.764860], 14).addControl(L.mapbox.shareControl()).addControl(L.mapbox.geocoderControl('mapbox.places'));
 	that.map = map;
 
-	that.gotoloc = '';
+	var todayDateString;
+	var d = new Date(); 
+
+	todayDateString = d.toString("dddd, MMMM dd, yyyy h:mm tt");
+
+	that.gotoloc = [];
+	that.loader = document.getElementById('loader');
+	
 	var points = L.mapbox.featureLayer('sedonachamber.pmj9fija').addTo(map);
 
 	points.on('mouseover', function (e) {
 	  	e.layer.openPopup();
-	  	that.gotoloc = [e.layer.feature.geometry.coordinates[1],e.layer.feature.geometry.coordinates[0]];
+	  	
+	  	//make sure it doesn't take a line type
+	  	if(e.layer.feature.geometry.type == 'Point'){
+	  		that.gotoloc = [e.layer.feature.geometry.coordinates[1],e.layer.feature.geometry.coordinates[0]];	
+	  	}
 
 		$("#direc" ).click(function(e) {
 			e.preventDefault();
+			that.loader.className = '';
 		  	//get direction same
+		  	that.map.stopLocate();
 		    that.map.locate();
 		});
 	});
 	
-	// var mylocIcon = L.icon({
-	//     iconUrl: 'app/css/45.gif',
-	//     iconAnchor: [8, 8]
-	// });
+	var mylocIcon = L.icon({
+	     iconUrl: 'app/css/ripple.gif',
+	     iconAnchor: [10, 10]
+	 });
 
 	var lc = L.control.locate({follow: true,
 		keepCurrentZoomLevel: false,
 		locateOptions: {maxZoom: 16},
 		metric: false,
 		showPopup: true, 
-		//markerClass: L.marker,
-		//markerStyle: {icon:mylocIcon},
-	  	onLocationError: function(err) {alert(err.message)}
+		markerClass: L.marker,
+		markerStyle: {icon:mylocIcon},
+	  	onLocationError: function(err) {
+	  		vex.dialog.buttons.YES.text = 'OK';
+
+	  		var howto = '';
+
+	  		if(bowser.msie){
+	  			howto = "<a target='_blank' style='color:#7A1800' href='http://windows.microsoft.com/en-us/internet-explorer/ie-security-privacy-settings#ie=ie-11'>How To </a>";	
+	  		}
+	  		else if(bowser.chrome){
+	  			howto = "<a target='_blank' style='color:#7A1800' href='https://support.google.com/chrome/answer/142065?hl=en'>How To </a>";	
+	  		}
+	  		else if(bowser.firefox){
+	  			howto = "<a target='_blank' style='color:#7A1800' href='https://support.mozilla.org/en-US/kb/improve-mozilla-location-services-turning-location'>How To </a>";	
+	  		}
+	  		else if(bowser.android){
+	  			howto = "<a target='_blank' style='color:#7A1800' href='https://support.scruff.com/hc/en-us/articles/202623634-How-do-I-enable-location-services-on-my-Android-'>How To </a>";	
+	  		}
+	  		else if(bowser.ios){
+	  			howto = "<a target='_blank' style='color:#7A1800' href='https://support.apple.com/en-us/HT203033'>How To </a>";	
+	  		}
+	  		
+	  		vex.dialog.alert({
+	            message: "Please Enable Your Location to find best directions from where you are.<br><br>"+howto
+	        });
+
+	  		that.loader.className = 'hide';
+	  	}
 	}).addTo(map);
 
-	L.control.layers({
-	    'Streets': L.mapbox.tileLayer('mapbox.streets',{maxZoom:20}),
-	    'Imagery': L.mapbox.tileLayer('mapbox.satellite', {maxZoom:20}),
-	    'Simple Streets': L.tileLayer('https://api.mapbox.com/styles/v1/sedonachamber/cin2opt8d00b9abnq6trki27e/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2Vkb25hY2hhbWJlciIsImEiOiJjaW13Zmp3cGswMzd0d2tsdXBnYmVjNmRjIn0.PlcjviLrxQht-_tBEbQQeg', {maxZoom:20}),
-	    'Outside<img src="http://sedona.simpleviewcrm.com/images//listings/celeste-800x600.jpg" height="160" width="160">': L.mapbox.tileLayer('mapbox.run-bike-hike'),
-	    'Sedona': L.tileLayer('https://api.mapbox.com/styles/v1/sedonachamber/cin7oyyjz000waamcx7v412nr/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2Vkb25hY2hhbWJlciIsImEiOiJjaW13Zmp3cGswMzd0d2tsdXBnYmVjNmRjIn0.PlcjviLrxQht-_tBEbQQeg', {maxZoom:20}),
-	    'Red': L.tileLayer('https://api.mapbox.com/styles/v1/sedonachamber/cin2kt8ku001sb4mawvdvwjxf/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2Vkb25hY2hhbWJlciIsImEiOiJjaW13Zmp3cGswMzd0d2tsdXBnYmVjNmRjIn0.PlcjviLrxQht-_tBEbQQeg', {maxZoom:20}).addTo(map),
-	    'Light': L.mapbox.tileLayer('mapbox.light'),
-	    'Dark': L.tileLayer('https://api.mapbox.com/styles/v1/sedonachamber/cin961o3z00epcxnhaxgzwdb6/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2Vkb25hY2hhbWJlciIsImEiOiJjaW13Zmp3cGswMzd0d2tsdXBnYmVjNmRjIn0.PlcjviLrxQht-_tBEbQQeg', {maxZoom:20})
-	}, {
-		//add other layers here
-	    //'Bike Stations': L.mapbox.tileLayer('examples.bike-locations')
-	}).addTo(map);
-
+	//turn dark if at night
+	if(d.getHours()>19||d.getHours()<7){
+		L.control.layers({
+		    'Streets': L.mapbox.tileLayer('mapbox.streets',{maxZoom:20}),
+		    'Imagery': L.mapbox.tileLayer('mapbox.satellite', {maxZoom:20}),
+		    'Simple Streets': L.tileLayer('https://api.mapbox.com/styles/v1/sedonachamber/cin2opt8d00b9abnq6trki27e/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2Vkb25hY2hhbWJlciIsImEiOiJjaW13Zmp3cGswMzd0d2tsdXBnYmVjNmRjIn0.PlcjviLrxQht-_tBEbQQeg', {maxZoom:20}),
+		    'Outside': L.mapbox.tileLayer('mapbox.run-bike-hike'),
+		    'Sedona': L.tileLayer('https://api.mapbox.com/styles/v1/sedonachamber/cin7oyyjz000waamcx7v412nr/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2Vkb25hY2hhbWJlciIsImEiOiJjaW13Zmp3cGswMzd0d2tsdXBnYmVjNmRjIn0.PlcjviLrxQht-_tBEbQQeg', {maxZoom:20}),
+		    'Red': L.tileLayer('https://api.mapbox.com/styles/v1/sedonachamber/cin2kt8ku001sb4mawvdvwjxf/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2Vkb25hY2hhbWJlciIsImEiOiJjaW13Zmp3cGswMzd0d2tsdXBnYmVjNmRjIn0.PlcjviLrxQht-_tBEbQQeg', {maxZoom:20}),
+		    'Light': L.mapbox.tileLayer('mapbox.light'),
+		    'Dark': L.tileLayer('https://api.mapbox.com/styles/v1/sedonachamber/cin961o3z00epcxnhaxgzwdb6/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2Vkb25hY2hhbWJlciIsImEiOiJjaW13Zmp3cGswMzd0d2tsdXBnYmVjNmRjIn0.PlcjviLrxQht-_tBEbQQeg', {maxZoom:20}).addTo(map)
+		}, {
+			//add other layers here
+		    //'Bike Stations': L.mapbox.tileLayer('examples.bike-locations')
+		}).addTo(map);
+	}
+	else{
+		L.control.layers({
+		    'Streets': L.mapbox.tileLayer('mapbox.streets',{maxZoom:20}),
+		    'Imagery': L.mapbox.tileLayer('mapbox.satellite', {maxZoom:20}),
+		    'Simple Streets': L.tileLayer('https://api.mapbox.com/styles/v1/sedonachamber/cin2opt8d00b9abnq6trki27e/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2Vkb25hY2hhbWJlciIsImEiOiJjaW13Zmp3cGswMzd0d2tsdXBnYmVjNmRjIn0.PlcjviLrxQht-_tBEbQQeg', {maxZoom:20}),
+		    'Outside': L.mapbox.tileLayer('mapbox.run-bike-hike'),
+		    'Sedona': L.tileLayer('https://api.mapbox.com/styles/v1/sedonachamber/cin7oyyjz000waamcx7v412nr/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2Vkb25hY2hhbWJlciIsImEiOiJjaW13Zmp3cGswMzd0d2tsdXBnYmVjNmRjIn0.PlcjviLrxQht-_tBEbQQeg', {maxZoom:20}),
+		    'Red': L.tileLayer('https://api.mapbox.com/styles/v1/sedonachamber/cin2kt8ku001sb4mawvdvwjxf/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2Vkb25hY2hhbWJlciIsImEiOiJjaW13Zmp3cGswMzd0d2tsdXBnYmVjNmRjIn0.PlcjviLrxQht-_tBEbQQeg', {maxZoom:20}).addTo(map),
+		    'Light': L.mapbox.tileLayer('mapbox.light'),
+		    'Dark': L.tileLayer('https://api.mapbox.com/styles/v1/sedonachamber/cin961o3z00epcxnhaxgzwdb6/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2Vkb25hY2hhbWJlciIsImEiOiJjaW13Zmp3cGswMzd0d2tsdXBnYmVjNmRjIn0.PlcjviLrxQht-_tBEbQQeg', {maxZoom:20})
+		}, {
+			//add other layers here
+		    //'Bike Stations': L.mapbox.tileLayer('examples.bike-locations')
+		}).addTo(map);
+	}
+	
+	that.loader.className = 'hide';
 	map.on('locationfound',(function(t) {
 
         //find closest point
@@ -73,7 +132,7 @@ function startup(){
         that.currentloc = loc;
 
         //for locate option
-        if(that.gotoloc ==''){
+        if(that.gotoloc.length==0){
         	var jsonpoints = points.getGeoJSON();
 	        ///filter out points
 	        for (var i = 0; i < jsonpoints.features.length; i++) {
@@ -81,15 +140,16 @@ function startup(){
 	            jsonpoints.features.splice(i, 1);
 	          }
 	        }
-
-	        var nearestpoint = turf.nearest(loc, jsonpoints);
+	        
+	        var nearestpoint = L.GeometryUtil.closestLayer(that.map,points,t.latlng);
+	        //var nearestpoint = turf.nearest(loc, jsonpoints);
 	        //nearestpoint.properties['marker-color'] = '#58595B';
 	        //nearestpoint.properties['marker-size'] = 'large';
 	        //points.setGeoJSON(jsonpoints);
 	        //points._geojson.features.concat(nearestpoint);
 	        
 	        points.eachLayer(function (layer) {
-			    if (layer.feature.properties.title === nearestpoint.properties.title) {
+			    if (layer.feature.properties.title === nearestpoint.layer.feature.properties.title) {
 			      	//var tomarker = layer.toGeoJSON();
 			      	layer.openPopup();
 			    };
@@ -97,22 +157,30 @@ function startup(){
 
 	        vex.dialog.buttons.YES.text = 'Get Directions';
 	        vex.dialog.alert({
-	            message: "You are here!<br><br><i>"+nearestpoint.properties.title+"</i> is the closest to you.<br>",
+	            message: "You are here!<br><br><i>"+nearestpoint.layer.feature.properties.title+"</i> is closest to you.<br>",
 	            callback: function(value) {
 	              //get directions  
 	              //to
-	              var tocoords=[nearestpoint.geometry.coordinates[1],nearestpoint.geometry.coordinates[0]];
+	              var tocoords=[nearestpoint.layer.feature.geometry.coordinates[1],nearestpoint.layer.feature.geometry.coordinates[0]];
 	              
+				  that.loader.className = '';
 				  that.getdirections(t.latlng,tocoords);
 	            }
 	        });
         }
         //for regular directions
         else{
-        	that.getdirections(t.latlng,that.gotoloc);
+        	//this shouldn't happen anymore based on the lines
+        	if( Object.prototype.toString.call( that.gotoloc[0] ) === '[object Array]' ) {
+        		that.getdirections(t.latlng,that.gotoloc[0]);	
+        	}
+        	else{
+        		that.getdirections(t.latlng,that.gotoloc);		
+        	}
+        	
         }
         
-        map.stopLocate();
+        that.map.stopLocate();
         //lc.stop(); 
 	}));
 
@@ -147,10 +215,6 @@ function startup(){
 	vex.dialog.buttons.YES.text = 'Start Here';
 	vex.dialog.buttons.NO.text = 'Browse Map';
 
-	var todayDateString;
-	var d = new Date(); 
-
-	todayDateString = d.toString("dddd, MMMM dd, yyyy h:mm tt");
 	if(d.getDay() == 5&&d.getDate()<7){
 	  todayDateString= todayDateString+'<br><br><a target="_blank" style="color:#7A1800" href="http://sedonagalleryassociation.com/?page_id=236">First Friday Art Walk. Click here for more information.</a></i>'
 	  
@@ -164,7 +228,7 @@ function startup(){
 	}
 
 	vex.dialog.confirm({
-	  message: "Welcome to the Sedona ArtWalk Map!<br><br><img src='assets/red_sedona.jpg' style='margin-left: 29%'></><br><br>"+todayDateString,
+	  message: "Welcome to the Sedona Art Walk Map!<br><br><a target='_blank' href='http://www.sedonachamber.com/'><img src='assets/red_sedona.jpg' style='padding-bottom:20px !important;padding-right:32px !important;'></></a><a target='_blank' href='http://sedonagalleryassociation.com/'><img src='assets/sga-logo-web.jpg' style=''></></a><br><br>"+todayDateString,
 	  callback: function(value) {
 	    //locate for directions
 	    if(value == true){
@@ -178,8 +242,8 @@ function startup(){
 
 function getdirections(start,end){
 	that = this;
-
-	//duration in seconds/60
+	
+	//duration in seconds/60 more because its cycling
 	//distance in meters/3.28084 ft
 	//distance in meters/0.000621371 miles
 
@@ -193,14 +257,14 @@ function getdirections(start,end){
 		that.map.removeLayer(that.from)
 	}
 	
-	that.to = L.circle(end, 25,{
+	that.to = L.circle(end, 23,{
 		    color: '#fff',
 		    fillColor: '#9E2C2E',
 		    fillOpacity: 0.7
 	   }).addTo(map);  
 
 	//from 
-	that.from = L.circle(start, 25,{
+	that.from = L.circle(start, 23,{
 		    color: '#fff',
 		    fillColor: '#A1D490',
 		    fillOpacity: 0.7
@@ -218,23 +282,32 @@ function getdirections(start,end){
 		    directions.properties = {};
 		    
 		    //directions.properties.popupContent		    
-		    var customPopup= "<i>Walking Time: "+Number(data.routes[0].duration/19).toPrecision(2)+" minutes<br><br>Walking Distance: "+Number(data.routes[0].distance*0.000621371).toPrecision(2)+" miles";
+		    var customPopup= "<i>Walking Time: "+Number(data.routes[0].duration/19).toPrecision(3)+" minutes<br><br>Walking Distance: "+Number(data.routes[0].distance*0.000621371).toPrecision(2)+" miles";
 			
 			var dirStyle = {
 			    "color": "#61C5BE",
 			    "weight": 5,
+			    "dashArray": '5,10',
 			    "opacity": 0.9
 			};
 
 			that.directionlayer = L.geoJson(directions,dirStyle).bindPopup(customPopup).addTo(that.map);
+			
 			that.directionlayer.eachLayer(function(m) {
 			  m.openPopup();
 			});
+
+			//zoom to
+			var bounds = that.directionlayer.getBounds();
+			map.fitBounds(bounds);
 		}
 		else{
-			alert('No Current Directions, please try again')
+			vex.dialog.alert({
+	            message: "No Current Directions, please try again."
+	        });
 		}	
 
-		that.gotoloc = '';  
+		that.gotoloc = [];  
+		that.loader.className = 'hide';
 	});
 }
