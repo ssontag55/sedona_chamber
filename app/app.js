1f16f68c0827a8c7400f6ca85555456878ea5a4c
pigeontoe <electriclady1@gmail.com>
2019-09-18 16:06:24 -0400

    better side menu open & close functionality

--- app/app.js
+++ app/app.js
@@ -822,12 +822,8 @@ function getdirections(start,end){
 	});
 }
 
-function openNav() {
-  document.getElementById("side-menu").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeNav() {
-  document.getElementById("side-menu").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+function toggleNav() {
+  $('#side-menu').toggleClass('is-active');
+  $('#hamburger').toggleClass('is-active');
+  $('#main').toggleClass('is-active');
 }
